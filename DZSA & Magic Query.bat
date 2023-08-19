8a278b9e7829f1cf835a57a058e7243832fe4578
Nick <90505328+napswastaken@users.noreply.github.com>
2023-08-19 15:38:21 +1200

    Commit

--- DZSA & Magic Query.bat
+++ DZSA & Magic Query.bat
@@ -1,4 +1,12 @@
 @ECHO OFF
+REM ***********************************************************************
+REM Description: This script prevents DZSA and Magic Launcher from removing servers by querying the server list every 10 minutes and sending a message to a Discord webhook with the time and date.
+REM Note: Due to JSON formatting limitations in Batch scripting, %API_RESPONSE% cannot be sent to Discord. Therefore, while a message is sent to Discord, it does not guarantee a successful API response.
+REM Copyright: Copyright (C) 2023 Nick Shepherd
+REM License: General Public License v3.0
+REM Version: 1.0
+REM ***********************************************************************
+
 REM Make sure to set the SERVERIP, QUERYPORT & WEBHOOK_URL
 SET SERVERIP=
 SET QUERYPORT=
@@ -13,20 +21,18 @@ FOR /F "tokens=1-3 delims= " %%a IN ('powershell.exe -Command "Get-Date -Format 
     SET "DATETIME=%%a %%b %%c"
 )
 REM Send DZSA API Alert to Discord Webhook
-SET "JSON={\"content\":\"[Development] DZSA API queried at %DATETIME%\"}"
+SET "JSON={\"content\":\"DZSA API queried at %DATETIME%\"}"
 curl -H "Content-Type: application/json" -d "%JSON%" %WEBHOOK_URL%
-
 
 ECHO Querying Server %SERVERIP%:%QUERYPORT%
 ECHO API Response: %API_RESPONSE%
 ECHO Query DateTime: %DATETIME%
 
-
 REM Query Magic Launcher API
 FOR /F "tokens=*" %%j IN ('powershell.exe -Command "(new-object System.Net.WebClient).DownloadString('https://api.dayzmagiclauncher.com/servers/%SERVERIP%:%QUERYPORT%')"') DO (
     SET "MAGIC_API_RESPONSE=%%j"
 )
-SET "JSON={\"content\":\"[Development] Magic Launcher API queried at %DATETIME%\"}"
+SET "JSON={\"content\":\"Magic Launcher API queried at %DATETIME%\"}"
 
 REM Send Magic Launcher API Alert to Discord Webhook
 curl -H "Content-Type: application/json" -d "%JSON%" %WEBHOOK_URL%
