8797c570f7f602113978e858343077226daf7cba
Nick <90505328+napswastaken@users.noreply.github.com>
2023-10-31 13:44:49 +1300

    Update DZSA & Magic Query.bat

--- DZSA & Magic Query.bat
+++ DZSA & Magic Query.bat
@@ -1,7 +1,6 @@
 @ECHO OFF
 REM ***********************************************************************
-REM Description: This script prevents DZSA and Magic Launcher from removing servers by querying the server list every 10 minutes and sending a message to a Discord webhook with the time and date.
-REM Note: Due to JSON formatting limitations in Batch scripting, %API_RESPONSE% cannot be sent to Discord. Therefore, while a message is sent to Discord, it does not guarantee a successful API response.
+REM Description: This script prevents DZSA and Magic Launcher from removing servers by querying the server list every 10 minutes.
 REM Copyright: Copyright (C) 2023 Nick Shepherd
 REM License: General Public License v3.0
 REM Version: 1.0
@@ -10,7 +9,6 @@ REM ***********************************************************************
 REM Make sure to set the SERVERIP, QUERYPORT & WEBHOOK_URL
 SET SERVERIP=
 SET QUERYPORT=
-SET WEBHOOK_URL=
 
 :START
 REM Query DZSA API
@@ -20,9 +18,6 @@ FOR /F "tokens=*" %%i IN ('powershell.exe -Command "(new-object System.Net.WebCl
 FOR /F "tokens=1-3 delims= " %%a IN ('powershell.exe -Command "Get-Date -Format 'yyyy-MM-dd HH:mm:ss'"') DO (
     SET "DATETIME=%%a %%b %%c"
 )
-REM Send DZSA API Alert to Discord Webhook
-SET "JSON={\"content\":\"DZSA API queried at %DATETIME%\"}"
-curl -H "Content-Type: application/json" -d "%JSON%" %WEBHOOK_URL%
 
 ECHO Querying Server %SERVERIP%:%QUERYPORT%
 ECHO API Response: %API_RESPONSE%
@@ -32,13 +27,10 @@ REM Query Magic Launcher API
 FOR /F "tokens=*" %%j IN ('powershell.exe -Command "(new-object System.Net.WebClient).DownloadString('https://api.dayzmagiclauncher.com/servers/%SERVERIP%:%QUERYPORT%')"') DO (
     SET "MAGIC_API_RESPONSE=%%j"
 )
-SET "JSON={\"content\":\"Magic Launcher API queried at %DATETIME%\"}"
+SET "JSON={\"content\":\"[Development] Magic Launcher API queried at %DATETIME%\"}"
 
-REM Send Magic Launcher API Alert to Discord Webhook
-curl -H "Content-Type: application/json" -d "%JSON%" %WEBHOOK_URL%
-ECHO Magic Launcher API Response: %MAGIC_API_RESPONSE%
 
 REM Clear console, query again in 10 minutes.
-TIMEOUT 600
+TIMEOUT 3600
 cls
 GOTO START
